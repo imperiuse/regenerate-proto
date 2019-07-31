dd2b52f71411072756aa6bc550e87731d5f3c62e
asazanov <asazanov@beget.ru>
2019-07-31 18:36:04 +0300

    fix example, fix flags, add output flag

--- regenerate-proto.py
+++ regenerate-proto.py
@@ -9,8 +9,9 @@ def parse_args():
         formatter_class=argparse.RawDescriptionHelpFormatter,
         description='Compile protobufs to python pb')
     parser.add_argument('--proto_dir', type=str, help='directory with proto files', default=os.curdir)
-    parser.add_argument('--python', type=bool, help='generate python code', default=False)
-    parser.add_argument('--golang', type=bool, help='generate golang code', default=False)
+    parser.add_argument('--out_dir', type=str, help='output dir for generated code', default=".")
+    parser.add_argument('--python', dest='python', action='store_true')
+    parser.add_argument('--golang', dest='golang', action='store_true')
     return parser.parse_args()
 
 
@@ -39,8 +40,9 @@ def get_proto(root: str):
 if __name__ == "__main__":
     args = parse_args()
     print(f"Dir for search .proto files: {args.proto_dir}")
-    print(f"Generate ccode for Python: {args.python}")
-    print(f"Generate ccode for Golang: {args.golang}")
+    print(f"Output dir: {args.out_dir}")
+    print(f"Generate code for Python: {args.python}")
+    print(f"Generate code for Golang: {args.golang}")
 
     proto_files = get_proto(args.proto_dir)
     print(f"Found proto files: {proto_files}")
@@ -49,15 +51,15 @@ if __name__ == "__main__":
         if args.python:
             proto_build_cmd = PYTHON_PROTOC_TEMPLATE.format(
                 I="/".join(dir.split('/')[:-2]),
-                python_out=".",
-                grpc_python_out=".",
+                python_out=args.out_dir,
+                grpc_python_out=args.out_dir,
                 proto=os.path.join(dir, proto_file))
             print(f"[Python]: {proto_build_cmd}")
             os.system(proto_build_cmd)
         elif args.golang:
             proto_build_cmd = GOLANG_PROTOC_TEMPLATE.format(
                 I="/".join(dir.split('/')[:-2]),
-                golang_out=".",
+                go_out=args.out_dir,
                 proto=os.path.join(dir, proto_file))
             print(f"[Golang]: {proto_build_cmd}")
             os.system(proto_build_cmd)
--- regenerate_proto.sh
+++ regenerate_proto.sh
@@ -1,7 +1,9 @@
 #!/usr/bin/env bash
 
+mkdir -p ./proto
+
 echo "Generate Python *.pb"
-python3 ./regenerate-proto.py --proto_dir ./api --python
+python3 ./tools/regenerate-proto/regenerate-proto.py --proto_dir ./api --out_dir ./proto --python
 
 echo "Generate Golang *.pb"
-python3 ./regenerate-proto.py --proto_dir ./api --golang
+python3 ./tools/regenerate-proto/regenerate-proto.py --proto_dir ./api --out_dir ./proto --golang
